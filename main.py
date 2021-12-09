58d335bc77bcd730df472ff6fe3f8cce1bcf59da
Misaa <90842745+Just-Misaa@users.noreply.github.com>
2021-12-09 13:54:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import discord
-import asyncio
+import asyncio, io
+import config
+
+import time
+import requests
 import os
 import random
 
+import anime_images_api
 
+from random import randint
+from config import *
+from datetime import datetime
 from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions, CheckFailure
 from itertools import cycle
 
 
 client = commands.Bot(command_prefix = '.')
 status = cycle(['Hololive', '.help'])
+anime = anime_images_api.Anime_Images()
+
+
+
 client.remove_command('help')
 client.remove_command('add')
 client.remove_command('sub')
@@ -59,22 +72,37 @@ async def ping(ctx):
 @client.command(aliases = ["8ball", '8b'])
 async def _8ball(ctx, *, question):
         answers = [
-          'Yes',
-          'No',
-          "I don't know...",
-          #You can just add more the answer that you want to make the bot do a random and pick it up by the syntax below
-          #"the answer here",
-          #and remember that THE LAST ANSWERS in this dict do not have the ','
-          
-          #example
-          #"this is the last answer"
+            "Yes.",
+            "Nah i don't know",
+            "No.",
+            "Nope",
+            "Yea!",
+            'Maybe...',
+            "Not now, im busy...",
+            "I love you",
+            "I hate you dumbass",
+            "Breh",
+            "Lmao.",
+            "I will not answer this question.",
+            "Damn.",
+            "Can you write more clearly?",
+            "Hmm...",
+            "My source code say 'Yes'!!",
+            "My source code say 'No' :<",
+            "Take it easy dude.",
+            "No horny",
+            "Calm",
+            "Do you watch Hololive? If not, don't ask me.",
+            "GAWR!",
+            "I love Gura",
+            "I love Ame",
+            "Will you marry me?"
 
 
         ]
         await ctx.send(f"Question: {question}\nAnswer: {random.choice(answers)} ")
 
 
-      
 @client.command(aliases = ['cr'])
 async def coolrate(ctx):
     embed = discord.Embed(title = 'Cool Rate', description = f"{ctx.author.mention} is {random.randrange(101)}% cool!", color = discord.Color.random())
@@ -155,8 +183,55 @@ async def unban(ctx, *, member):
 
 
 
+
+
+
+
+
+
+
+
+#   OTHERS COMMANDS
+
+@client.command()
+async def hentai(ctx):
+    nsfw = anime.get_nsfw('hentai')
+    embed = discord.Embed(
+        title = "Hentai",
+        description = "You are lewd.",
+        color = discord.Color.random()
+    )
+
+    embed.set_footer(text = "Follow me on instagram by `@foxymisa1102`!")
+    embed.set_image(url = nsfw)
+    await ctx.send(embed = embed)
+
+
+
+@client.command()
+async def hug(ctx, *, member : discord.Member = None):
+
+
+    sfw = anime.get_sfw('hug')
+    embed = discord.Embed(title = "Hug Command", description = "Hugged you", color = discord.Color.random())
+    embed.set_image(url = sfw)
+    await ctx.send(embed = embed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #    HELPS
-#i am a newbie to discord.py, so i dont know how to make auto collect commands, so you just need to ADD commands by YOURSELF
+
 
 @client.command()
 async def help(ctx):
@@ -165,8 +240,9 @@ async def help(ctx):
     helpEmbed.add_field(name = "Fun", value = "`8ball`, `cool rate`, `ping`", inline = False)
     helpEmbed.add_field(name = "Math", value = "`sum`, `subtract`, `multi`, `division`", inline = False)
     helpEmbed.add_field(name = "Moderation", value = "`clear`, `kick`, `ban`, `unban`", inline = False)
-
+    helpEmbed.add_field(name = "Utils", value = "`hug`, `hentai`", inline = False)
     await ctx.send(embed = helpEmbed)
+
 
 
 
