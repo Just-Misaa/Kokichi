ade83e4a6d6032dc76e14a704c05d96f4ef368c4
Misaa <90842745+Just-Misaa@users.noreply.github.com>
2021-12-12 19:33:25 +0700

    add mute, activity commands

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import discord
 import asyncio, io
 import config
+import json
 
 import time
 import requests
 import os
 import random
+import queue
+import threading
+import sys
 
 import anime_images_api
 
-from random import randint
+
+
+from random import randint, choice
+
 from config import *
+
+
 from datetime import datetime
+from discord import Guild, Member, Embed
+from discord.utils import get
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, CheckFailure
 from itertools import cycle
 
 
+
+
+
+
+
+
 client = commands.Bot(command_prefix = '.')
-status = cycle(['Hololive', '.help'])
-anime = anime_images_api.Anime_Images()
-
-
 
 client.remove_command('help')
 client.remove_command('add')
@@ -29,9 +42,23 @@ client.remove_command('sub')
 client.remove_command('div')
 client.remove_command('mul')
 
+status = cycle(['Hololive', '.help', 'VALORANT'])
+anime = anime_images_api.Anime_Images()
+
+
+
+
+
+
+
+
+
+
+
 @client.event
 async def on_ready():
     print('Kokichi is online!')
+
     
 @tasks.loop(seconds = 120)
 async def change_status():
@@ -118,6 +145,7 @@ async def coolrate(ctx):
 
 
 
+
 #      MATH COMMANDS
 
 @client.command(aliases = ['sum'])
@@ -139,6 +167,8 @@ async def mul(ctx, left : int, right : int):
 async def div(ctx, left : int, right : int):
     sum4 = left / right
     await ctx.send("The answer is: " + str(sum4))
+
+
 
 
 
@@ -183,16 +213,87 @@ async def unban(ctx, *, member):
 
 
 
+@client.command()
+async def userinfo(ctx, member : discord.Member):
+
+    roles = [role for role in member.roles]
+
+    roles = []
+    for role in member.roles:
+        roles.append(role)
+
+
+    embed = discord.Embed(color = discord.Color.random(), timestamp = ctx.message.created_at)
+
+    embed.set_author(name = f"User Info - {member}")
+
+    embed.set_thumbnail(url = member.avatar_url)
+
+    embed.add_field(name = "ID:", value = member.id)
+
+    embed.add_field(name = "Guild name:", value = member.display_name)
+
+    embed.add_field(name = "Created at:", value = member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+
+    embed.add_field(name = "Joined at:", value = member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+
+    embed.add_field(name = f"Roles ({len(roles)})", value = " ".join([role.mention for role in roles]))
+
+    embed.add_field(name = "Top role:", value = member.top_role.mention)
+
+    embed.add_field(name = "Bot?", value = member.bot)
+
+    await ctx.send(embed = embed)
+
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(manage_roles = True)
+async def create_mute_role(guild : Guild):
+    role_name = "muted"
+
+    mute_role = get(guild.roles, name = role_name)
+
+    if mute_role is None:
+        await guild.create_role(name = role_name)
+
+        mute_role = get(guild.roles, name = role_name)
+
+    for channel in guild.text_channels:
+        await asyncio.sleep(0)
+
+
+        mute_permissions = discord.PermissionOverwrite()
+        mute_permissions.send_messages = False
+
+        await channel.set_permissions(mute_role, overwrite = mute_permissions)
+
+    return (mute_role)
+
+
+@client.command(pass_context=True)
+@commands.has_permissions(manage_roles = True)
+async def mute(ctx, member: discord.Member):
+    guild = ctx.message.guild
+    mute_role = await create_mute_role(guild)
+    await member.add_roles(mute_role)
+
+    await ctx.send(f"{member.name} has been muted !")
+    return
 
 
 
 
+@client.command()
+@commands.has_permissions(administrator=True)
+async def activity(ctx, *, activity):
+    await client.change_presence(activity=discord.Game(name=activity))
+    await ctx.send(f"Bot's activity changed to {activity}")
 
 
 
 
 #   OTHERS COMMANDS
-
 @client.command()
 async def hentai(ctx):
     nsfw = anime.get_nsfw('hentai')
@@ -206,7 +307,10 @@ async def hentai(ctx):
     embed.set_image(url = nsfw)
     await ctx.send(embed = embed)
 
-
+# @client.command()
+# async def hug(ctx):
+#     sfw = anime.get_sfw('hug')
+#     await ctx.send(sfw)
 
 @client.command()
 async def hug(ctx, *, member : discord.Member = None):
@@ -219,12 +323,23 @@ async def hug(ctx, *, member : discord.Member = None):
 
 
 
+async def get_photo():
+    res = requests.get("https://cultured.skellymclane386.repl.co/photo/random")
+    return res.json()
 
+@client.command()
+async def vtuber(ctx : commands.Context):
+    _id, url = await get_photo()
 
+    embed = discord.Embed(
+        title = f"Vtuber: {_id}",
+        color = discord.Color.random(),
+        url = url
+        )
+    embed.set_image(url = url)
+    embed.set_footer(text = "follow me on Instagram by `@foxymisa1102`!")
 
-
-
-
+    await ctx.send(embed = embed)
 
 
 
@@ -239,9 +354,13 @@ async def help(ctx):
 
     helpEmbed.add_field(name = "Fun", value = "`8ball`, `cool rate`, `ping`", inline = False)
     helpEmbed.add_field(name = "Math", value = "`sum`, `subtract`, `multi`, `division`", inline = False)
-    helpEmbed.add_field(name = "Moderation", value = "`clear`, `kick`, `ban`, `unban`", inline = False)
-    helpEmbed.add_field(name = "Utils", value = "`hug`, `hentai`", inline = False)
+    helpEmbed.add_field(name = "Moderation", value = "`clear`, `kick`, `ban`, `unban`, `userinfo`, `activity`, `mute`", inline = False)
+    helpEmbed.add_field(name = "Utils", value = "`hug`, `hentai`, `vtuber`", inline = False)
     await ctx.send(embed = helpEmbed)
+
+
+
+
 
 
 
